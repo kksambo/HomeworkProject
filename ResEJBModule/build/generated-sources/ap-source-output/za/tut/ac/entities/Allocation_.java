3f65fdef6360f11b5ad7300acf500a0d82030d65
sicelo <124036348+kksambo@users.noreply.github.com>
2025-05-12 12:21:45 +0200

    Add files via upload

---
+++ ResEJBModule/build/generated-sources/ap-source-output/za/tut/ac/entities/Allocation_.java
@@ -0,0 +1,16 @@
+package za.tut.ac.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-05-12T10:44:11")
+@StaticMetamodel(Allocation.class)
+public class Allocation_ { 
+
+    public static volatile SingularAttribute<Allocation, String> RoomNo;
+    public static volatile SingularAttribute<Allocation, String> studNo;
+    public static volatile SingularAttribute<Allocation, String> name;
+    public static volatile SingularAttribute<Allocation, Long> id;
+
+}
---
+++ ResEJBModule/build/generated-sources/ap-source-output/za/tut/ac/entities/ResManager_.java
@@ -0,0 +1,15 @@
+package za.tut.ac.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-05-12T10:44:11")
+@StaticMetamodel(ResManager.class)
+public class ResManager_ { 
+
+    public static volatile SingularAttribute<ResManager, String> password;
+    public static volatile SingularAttribute<ResManager, Long> id;
+    public static volatile SingularAttribute<ResManager, String> username;
+
+}
